b022e0b4f59a4b2af67090316302ba00a1a88de9
Florian MARGAINE <florian@margaine.com>
2015-03-07 14:05:58 +0100

    Returns the correct data in list_items()
    The query needs to return the statuses too, so that client-side can
    correctly sort the items in the list.

--- sql/list_items.sql
+++ sql/list_items.sql
@@ -1,9 +1,14 @@
 -- Gets all the items of a list
 
 CREATE OR REPLACE FUNCTION list_items(p_list_id INTEGER) RETURNS setof RECORD AS $$
-        SELECT i.name
+        SELECT i.name, l.status
         FROM item i
         LEFT JOIN list_items l ON i.id = l.item_id
-        WHERE l.list_id = p_list_id;
+        WHERE l.list_id = p_list_id
+        -- status 1 = new, 2 = bought, 3 = deleted.
+        -- Keeping deleted so that it's possible to do
+        -- statistics later on.
+        AND l.status != 3
+        ORDER BY l.status ASC, i.name ASC;
 $$
 LANGUAGE SQL;
--- src/routes/item.lisp
+++ src/routes/item.lisp
@@ -8,5 +8,5 @@
 ;;;; $1 is the item name, $2 is the list id
 (pm:defprepared add-item "select add_item($1, $2)" :single)
 
-;;;; Gets all the items in a list
+;;;; Gets all the items and their status in a list
 (pm:defprepared list-items "select list_items($1)" :rows)
